5ab3275e2dece3a2d329f27d862ab34c09e84fac
dinhan2610 <truongdinhan000@gmail.com>
2025-03-27 00:49:58 +0700

    add Category

--- src/pages/AdminPage/AdminOrder.jsx
+++ src/pages/AdminPage/AdminOrder.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./index.css";
+import "./AdminOrder.css";
 
 function AdminOrder() {
   const [orders, setOrders] = useState([]);
